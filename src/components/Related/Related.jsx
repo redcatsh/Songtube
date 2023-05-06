82c4a9f3874e9d7aaa6dfdd34b8b4a7710bd5a7d
redcatsh <redminnie56@naver.com>
2023-05-06 22:51:37 +0900

    [feat] related video

--- src/components/Related/Related.jsx
+++ src/components/Related/Related.jsx
@@ -1,5 +1,44 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { timeForToday } from "../../utils/util";
 
 export default function Related() {
-  return <div></div>;
+  const { keyword } = useParams();
+  const {
+    isLoading,
+    error,
+    data: related,
+  } = useQuery(["related", keyword], async () => {
+    return fetch(`data/related.json`).then((res) => res.json());
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) return <p>{error}</p>;
+
+  return (
+    <div>
+      <ul>
+        {related.items.map((item) => (
+          <Link to={`/${item.id.videoId}`} className="videoItem" key={item.id}>
+            <li>
+              <img
+                src={String(item.snippet.thumbnails.medium.url)}
+                alt="thumbnail"
+              />
+              <h5 className="font-medium title">{item.snippet.title}</h5>
+              <p className="text-slate-500 text-sm">
+                {item.snippet.channelTitle}
+              </p>
+              <p className="text-slate-500 text-sm">
+                {timeForToday(item.snippet.publishTime)}
+              </p>
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import Related from "../components/Related/Related";
 export default function VideoDetail() {
   const { videoId } = useParams();
   return (
     <div>
-      <iframe
-        id="player"
-        type="text/html"
-        width="640"
-        height="390"
-        src={`http://www.youtube.com/embed/${videoId}?`}
-        frameborder="0"
-      ></iframe>
+      <div>
+        <iframe
+          id="player"
+          type="text/html"
+          width="640"
+          height="390"
+          src={`http://www.youtube.com/embed/${videoId}?`}
+          frameborder="0"
+        ></iframe>
+      </div>
+      <Related />
     </div>
   );
 }
